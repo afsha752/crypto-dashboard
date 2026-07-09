8f8f0820852497fc905e2eeb8af49aca1b21c754
Afsha752 <afshachishti0102@gmail.com>
2026-07-09 20:19:07 +0530

    Initial commit - basic React app setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>🪙 Crypto Dashboard</h1>
+      </header>
+
+      <main>
+        <p>This is where our coin list will go.</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
